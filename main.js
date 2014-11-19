5f3cf58a9c60b20510bfcb2085c77da48122668a
Vladimir Grichina <vgrichina@componentix.com>
2014-11-19 04:32:55 +0200

    Hacked together some hipster cellular automata, it seems to show fashion waves

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ var container = d3.selectAll(".container");
 
 function updateCells(cells) {
     return cells.style("background-color", function(d) {
-        console.log("update", d);
-        return "hsl(" + d/30.0 * 360 + ",100%,50%)";
-    }).text(String)
+        return (d.style ? "rgba(255,0,0," : "rgba(0,0,255,") +
+            (d.hipster ? "0.5)" : "0.25)");
+    })
 }
 
 function updateData(data) {
@@ -23,4 +23,60 @@ function updateData(data) {
     return cells;
 }
 
-updateData([[1, 2, 3], [4, 5, 6], [8]])
+var data = [];
+for (var i = 0; i < 100; i++) {
+    data.push([]);
+    for (var j = 0; j < 100; j++) {
+        data[i].push({
+            //hipster: (i+j) % 2,
+            hipster: Math.random() > 0.2,
+            style: 0
+        });
+    }
+}
+
+updateData(data);
+
+function map2d(data, fn) {
+    return data.map(function(row, i) {
+        return row.map(function (d, j) {
+            return fn(d, i, j);
+        });
+    });
+}
+
+function percentOfSameStyleNeighbors(data, si, sj) {
+    var RADIUS = 10;
+    var total = 0;
+    var matching = 0;
+    for (var i = Math.max(si - RADIUS, 0); i < Math.min(si + RADIUS, data.length - 1); i++) {
+        for (var j = Math.max(sj - RADIUS, 0); j < Math.min(sj + RADIUS, data[i].length - 1); j++) {
+            total ++;
+            if (data[i][j].style == data[si][sj].style) {
+                matching++;
+            }
+        }
+    }
+    return matching * 1.0 / total;
+}
+
+function step() {
+    data = map2d(data, function(d, i, j) {
+        var sameStylePercent = percentOfSameStyleNeighbors(data, i, j);
+        if (d.hipster) {
+            sameStylePercent = 1.0 - sameStylePercent;
+        }
+
+        var shouldChange = sameStylePercent < 0.5 && Math.random() > 0.75;
+
+        return {
+            hipster: d.hipster,
+            style: (shouldChange ? !d.style : d.style)
+        }
+    });
+    updateData(data);
+}
+
+//step();
+
+setInterval(step, 200);
